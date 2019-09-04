1f775ee13148b4d00a0877326afd06010c4d7f04
erickpeiker-olwmo <erick@owlmo.com.br>
2019-09-04 13:34:18 -0300

    Conceitos básicos react - Exercícios

---
+++ exercicios-react/src/componentes/Contador.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+
+export default class Contador extends Component {
+
+    state = {
+        numero: this.props.numeroInicial
+    }
+
+    maisUm = () => {
+        this.setState({numero: this.state.numero + 1})
+    }
+
+    menosUm = () => {
+        this.alterarNumero(-1)
+        //this.setState({numero: this.state.numero - 1})
+    }
+
+    alterarNumero = diferenca => {
+        this.setState({
+            numero: this.state.numero + diferenca
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>Número: {this.state.numero}</div>
+                <button onClick={this.maisUm}>Inc</button>
+                <button onClick={this.menosUm}>Dec</button>
+                <button onClick={() => this.alterarNumero(10)}>Inc 10</button>
+                <button onClick={() => this.alterarNumero(-10)}>Dec 10</button>
+            </div>
+        )
+    }
+
+}
+
+// constructor (props) {
+//     super(props)
+//     this.maisUm = this.maisUm.bind(this)
+// }
+
+
+//<button onClick={() => this.maisUm()}>Inc</button>
---
+++ exercicios-react/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import PrimeiroComponente from './componentes/PrimeiroComponente'
+//import {CompA, CompB} from './componentes/DoisComponentes'
+//import MultiElementos from './componentes/MultiElementos'
+//import FamiliaSilva from './componentes/FamiliaSilva'
+// import Familia from './componentes/Familia'
+// import Membro from './componentes/Membro'
+//import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+//import Pai from './componentes/Pai'
+// import ComponenteClasse from './componentes/ComponenteClasse'
+// import Contador from './componentes/Contador'
+import Hook from './componentes/Hook'
+
+const elemento = document.getElementById('root');
+ReactDOM.render(
+    <div>
+        <Hook />
+        {/* <ComponenteClasse valor="Sou um componente de classe"/> */}
+
+       {/* <Pai /> */}
+
+        {/* <ComponenteComFuncao /> */}
+        
+        {/* <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+           <Membro nome="Mariana" sobrenome="Pereira" />
+        </Familia> */}
+        
+        {/*
+        <FamiliaSilva sobrenome="Silva"/>
+        <MultiElementos />
+        <PrimeiroComponente valor="Bom dia!" />
+        
+        <CompA valor="Eu sou A"></CompA>
+        <CompB valor="Eu sou B"></CompB>
+        */}
+
+    </div>, elemento);
